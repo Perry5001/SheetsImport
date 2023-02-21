2bd91be6eb27b7c637e337d366f75831771e9ff4
Ritvik Goyal <ritvikrgoyal@gmail.com>
2023-02-20 22:07:47 -0600

    NewLibrary

--- script.js
+++ script.js
@@ -2,32 +2,27 @@ const sheetID ="1irlmk_gDxKYofa0ygQ56wLWwN8ZSAlYBYubDYMUVCJM"
 const clientID = "363918754034-resujk2n1dvosni5q9st4u6jva9cn2l6.apps.googleusercontent.com"
 const APIKey = "AIzaSyDBO16jGXqg6QAjrsrjkTPgNDkb0Y-9USg"
 
-// Load the Google API client library
-gapi.load('client', initClient);
-
-function initClient() {
-  // Set up the API client with your client ID and API key
-  gapi.client.init({
-    apiKey: APIKey,
-    clientId: clientID,
-    discoveryDocs: ["https://sheets.googleapis.com/$discovery/rest?version=v4"],
-    scope: 'https://www.googleapis.com/auth/spreadsheets.readonly'
-  }).then(function() {
-    // Make an API call to retrieve data from a spreadsheet
-    gapi.client.sheets.spreadsheets.values.get({
-      spreadsheetId: sheetID,
-      range: 'PLayoffs!A1:L17' // the range of cells you want to retrieve
+// Load the Google Identity Services library
+gapi.load('auth2', function() {
+  // Initialize the library with your client ID
+  gapi.auth2.init({
+    client_id: clientID,
+  }).then(function(authInstance) {
+    // Get the user's ID token
+    var user = authInstance.currentUser.get();
+    var idToken = user.getAuthResponse().id_token;
+    
+    // Make a request to the Google Sheets API using the ID token
+    fetch('https://sheets.googleapis.com/v4/spreadsheets/' + sheetID + '/values/Playoff!A1:L10', {
+      headers: {
+        Authorization: 'Bearer ' + idToken
+      }
     }).then(function(response) {
-      var data = response.result.values; // the data from the spreadsheet
-
-      // Convert the data to a JavaScript array
-      var arrayData = data.map(function(row) {
-        return [row[0], row[1], row[2]]; // adjust this based on the number of columns in your spreadsheet
-      });
-
-      console.log(arrayData); // display the array in the console
-    }, function(reason) {
-      console.error('error: ' + reason.result.error.message);
+      return response.json();
+    }).then(function(data) {
+      console.log('Data:', data);
+    }).catch(function(error) {
+      console.error('Error:', error);
     });
   });
-}
+});
