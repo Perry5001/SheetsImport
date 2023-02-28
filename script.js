73dda432f32337c9a71c1db4031da81b40130f9f
Ritvik Goyal <ritvikrgoyal@gmail.com>
2023-02-27 18:16:03 -0600

    PapaParse

--- script.js
+++ script.js
@@ -1,28 +1,8 @@
-const sheetID ="1irlmk_gDxKYofa0ygQ56wLWwN8ZSAlYBYubDYMUVCJM"
-const clientID = "363918754034-resujk2n1dvosni5q9st4u6jva9cn2l6.apps.googleusercontent.com"
-const APIKey = "AIzaSyDBO16jGXqg6QAjrsrjkTPgNDkb0Y-9USg"
+const url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSMVyT1bIeWOlazjgwn9U1gwW1uZLU2VxH-IdCjOOvKOP5tGSwg3g08IUYM-ubYAUibfskKxaDbjnZY/pubhtml"
 
-// Load the Google Identity Services library
-gapi.load('auth2', function() {
-  // Initialize the library with your client ID
-  gapi.auth2.init({
-    client_id: clientID,
-  }).then(function(authInstance) {
-    // Get the user's ID token
-    var user = authInstance.currentUser.get();
-    var idToken = user.getAuthResponse().id_token;
-    
-    // Make a request to the Google Sheets API using the ID token
-    fetch('https://sheets.googleapis.com/v4/spreadsheets/' + sheetID + '/values/Playoffs!A1:L10', {
-      headers: {
-        Authorization: 'Bearer ' + idToken
-      }
-    }).then(function(response) {
-      return response.json();
-    }).then(function(data) {
-      console.log('Data:', data);
-    }).catch(function(error) {
-      console.error('Error:', error);
-    });
+fetch(url)
+  .then(response => response.text())
+  .then(data => {
+    const parsedData = Papa.parse(data, { header: true }).data;
+    console.log(parsedData);
   });
-});
---
+++ script_GAPI.js
@@ -0,0 +1,28 @@
+const sheetID ="1irlmk_gDxKYofa0ygQ56wLWwN8ZSAlYBYubDYMUVCJM"
+const clientID = "363918754034-resujk2n1dvosni5q9st4u6jva9cn2l6.apps.googleusercontent.com"
+const APIKey = "AIzaSyDBO16jGXqg6QAjrsrjkTPgNDkb0Y-9USg"
+
+// Load the Google Identity Services library
+gapi.load('auth2', function() {
+  // Initialize the library with your client ID
+  gapi.auth2.init({
+    client_id: clientID,
+  }).then(function(authInstance) {
+    // Get the user's ID token
+    var user = authInstance.currentUser.get();
+    var idToken = user.getAuthResponse().id_token;
+    
+    // Make a request to the Google Sheets API using the ID token
+    fetch('https://sheets.googleapis.com/v4/spreadsheets/' + sheetID + '/values/Playoffs!A1:L10', {
+      headers: {
+        Authorization: 'Bearer ' + idToken
+      }
+    }).then(function(response) {
+      return response.json();
+    }).then(function(data) {
+      console.log('Data:', data);
+    }).catch(function(error) {
+      console.error('Error:', error);
+    });
+  });
+});
